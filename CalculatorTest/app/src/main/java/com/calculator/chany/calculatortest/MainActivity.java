6c594e017ab5a57306c6b887a8129d176368b85f
Chany Field <chantalfield08@gmail.com>
2017-05-04 01:25:13 -0400

    made text box and buttons

--- CalculatorTest/app/src/main/java/com/calculator/chany/calculatortest/MainActivity.java
+++ CalculatorTest/app/src/main/java/com/calculator/chany/calculatortest/MainActivity.java
@@ -4,8 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
-    // test for chany commit.
+    
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
