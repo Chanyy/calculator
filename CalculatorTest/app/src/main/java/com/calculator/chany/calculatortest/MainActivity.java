da24739086db206b9fd0a2577c6cee5e91e85876
Chany Field <chantalfield08@gmail.com>
2017-05-06 02:03:18 -0400

    Finish views

--- CalculatorTest/app/src/main/java/com/calculator/chany/calculatortest/MainActivity.java
+++ CalculatorTest/app/src/main/java/com/calculator/chany/calculatortest/MainActivity.java
@@ -2,6 +2,8 @@ package com.calculator.chany.calculatortest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     
@@ -10,5 +12,36 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Initializing Buttons
+        TextView numberTextView = (TextView) findViewById(R.id.number_text_view);
+        Button buttonOne = (Button) findViewById(R.id.button_one);
+        Button buttonTwo = (Button) findViewById(R.id.button_two);
+        Button buttonThree = (Button) findViewById(R.id.button_three);
+        Button buttonFour = (Button) findViewById(R.id.button_four);
+        Button buttonFive = (Button) findViewById(R.id.button_five);
+        Button buttonSix = (Button) findViewById(R.id.button_six);
+        Button buttonSeven = (Button) findViewById(R.id.button_seven);
+        Button buttonEight = (Button) findViewById(R.id.button_eight);
+        Button buttonNine = (Button) findViewById(R.id.button_nine);
+        Button buttonDecimal = (Button) findViewById(R.id.button_decimal);
+        Button ButtonEquals  = (Button) findViewById(R.id.button_equals);
+        Button ButtonClear = (Button) findViewById(R.id.button_clear);
+        Button ButtonDivide = (Button)findViewById(R.id.button_divide);
+        Button ButtonMultiply = (Button) findViewById(R.id.button_multiply);
+        Button ButtonSubtraction = (Button) findViewById(R.id.button_subtraction);
+        Button ButtonAddition = (Button) findViewById(R.id.button_addition);
+
+
+        whyThis()
     }
+
+    public void whyThis() {
+
+        int yunaAge = 99;
+        String yunaName = "Yuna the Cat";
+        yunaAge = yunaAge+1;
+        yunaName = "fatty";
+    }
+
 }
